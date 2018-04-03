6dd35e73103da2b419993e69835c8d18d869d43b
tnsherpa1 <tnsherpa1@gmail.com>
2018-04-03 09:07:22 -0700

    creates frontend routes using react router

--- users-ui/src/App.js
+++ users-ui/src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css'
+import Nav from './components/Nav'
+import UserNewForm from './components/UserNewForm'
 import UsersList from './components/UsersList'
-
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">User Management Application</h1>
+          <Nav/>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route exact path='/' component={UserNewForm} />
+          <Route exact path='/myusers' component={UsersList}/>
+          <Route render={ ()=><p>404 Not found</p> }/>
+        </Switch>
         <UsersList/>
       </div>
+      </Router>
     );
   }
 }
