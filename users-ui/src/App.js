bc12a2a39014a4f8eede584f814cc7106e606a39
tnsherpa1 <tnsherpa1@gmail.com>
2018-04-03 13:12:01 -0700

    moves state to routes and renders component from react router

--- users-ui/src/App.js
+++ users-ui/src/App.js
@@ -4,7 +4,34 @@ import './App.css'
 import Nav from './components/Nav'
 import UserNewForm from './components/UserNewForm'
 import UsersList from './components/UsersList'
+import axios from "axios";
+
 class App extends Component {
+  state = {
+    users: []
+  };
+
+  async componentDidMount(){
+    try {
+      const response = await axios.get(`/users/`)
+      this.setState({users: response.data})
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  createUser = async(user, index) => {
+    try {
+      const newUserResponse = await axios.post(`/users`, user)
+      const updatedUserResponse = [...this.state.users]
+      updatedUserResponse.push(newUserResponse.data)
+      this.setState({users: updatedUserResponse})
+    } catch(error) {
+      console.log("error creating new user")
+      console.log(error)
+    }
+  }
+
   render() {
     return (
       <Router>
@@ -14,11 +41,10 @@ class App extends Component {
           <Nav/>
         </header>
         <Switch>
-          <Route exact path='/' component={UserNewForm} />
-          <Route exact path='/myusers' component={UsersList}/>
+          <Route exact path='/' render={()=><UserNewForm createUser={this.createUser}/>} />
+          <Route exact path='/myusers' render={()=><UsersList users={this.state.users}/>} />
           <Route render={ ()=><p>404 Not found</p> }/>
         </Switch>
-        <UsersList/>
       </div>
       </Router>
     );
--- users-ui/src/components/UsersList.js
+++ users-ui/src/components/UsersList.js
@@ -4,39 +4,16 @@ import UserNewForm from './UserNewForm'
 import User from './User'
 
 class UsersList extends Component {
-  state = {
-    users: []
-  };
 
-  async componentDidMount(){
-    try {
-      const response = await axios.get(`/users/`)
-      this.setState({users: response.data})
-    } catch(error) {
-      console.log(error)
-    }
-  }
-
-  createUser = async(user, index) => {
-      try {
-        const newUserResponse = await axios.post(`/users`, user)
-        const updatedUserResponse = [...this.state.users]
-        updatedUserResponse.push(newUserResponse.data)
-        this.setState({users: updatedUserResponse})
-      } catch(error) {
-        console.log("error creating new user")
-        console.log(error)
-      }
-  }
   render() {
-    const userComponents =  this.state.users.map((user, index) => {
+    const userComponents = this.props.users.map((user, index) => {
       return <User key={index} username={user.userName} firstname={user.firstName} lastname={user.lastName}/>
     })
+
     return (
       <div>
         {userComponents}
         <hr/>
-        <UserNewForm createUser={this.createUser}/>
       </div>
     )
   }
